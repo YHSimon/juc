644b31a72831509dbaa9ce9ff536029d200c71ea
YHSimon <1981970494@qq.com>
2020-04-02 23:28:06 +0800

    '

--- juc_3/src/demo1/CountDownTest.java
+++ juc_3/src/demo1/CountDownTest.java
@@ -11,12 +11,20 @@ public class CountDownTest {
         //优先执行，执行完毕之后，才能执行main  (克制并发)
         //1. 实例化计数器 100
         CountDownLatch countDownLatch=new CountDownLatch(100);
+
+
         new Thread(()->{
             for (int i=0;i<100;i++){
                 System.out.println("++++++Thread");
-                countDownLatch.countDown();
+                //countDownLatch.countDown();
             }
         }).start();
+
+        //主线程
+        for (int i=0;i<100;i++){
+            countDownLatch.countDown();
+            System.out.println("main-------");
+        }
 
         //2.调用 await 方法  计算器不等于0,则后续线程一直等待;反之，计数器清零，后面的线程可以运行
         try {
@@ -25,10 +33,7 @@ public class CountDownTest {
             e.printStackTrace();
         }
 
-        //主线程
-        for (int i=0;i<100;i++){
-            System.out.println("main-------");
-        }
+
     }
 
 }
